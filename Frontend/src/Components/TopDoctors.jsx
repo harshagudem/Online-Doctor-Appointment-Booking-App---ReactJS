c6f3c301eb88978e1e21f86a12040826ce0b9994
Harsha <harsha.gudem@gmai.com>
2025-03-12 09:18:41 -0600

    All Doctors and Appointment pages

--- Frontend/src/Components/TopDoctors.jsx
+++ Frontend/src/Components/TopDoctors.jsx
@@ -13,7 +13,7 @@ const TopDoctors = () => {
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
         <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
             {doctors.slice(0,10).map((item,index) => (
-                <div  onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                <div onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                     <img className='w-full h-48 object-cover bg-blue-50' src={item.image} alt={item.name} />
                     <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-green-500'>
